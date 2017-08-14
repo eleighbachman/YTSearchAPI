f58196291b6f6cdf63538edb73c2459463b51722
Liz Bachman <eleighbachman@gmail.com>
2017-08-14 09:49:55 -0600

    Latest commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = 'AIzaSyAyIjK5SKO_OBwRSUY728L-65QjtMR_6s8';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log({ data });
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+
+
+App.propTypes = {
+  players: React.PropTypes.arrayOf(React.PropTypes.shape({
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired
+  })).isRequired,
+};
+
+//Then you have your <Application players = {players} /> where your variable of players is an object in the shape of name: 'string', score: 'number'.
