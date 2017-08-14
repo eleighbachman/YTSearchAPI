e6fa0a09640d13058f850587689fbe23970da16c
Liz Bachman <eleighbachman@gmail.com>
2017-08-14 09:54:40 -0600

    Adding State update from YT Search

--- src/index.js
+++ src/index.js
@@ -7,15 +7,18 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAyIjK5SKO_OBwRSUY728L-65QjtMR_6s8';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log({ data });
-});
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+
+
     this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+    })
 
 
   }
